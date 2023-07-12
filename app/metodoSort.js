b67decabb57921d381acbcecae115ad9937e5d31
Kevin Lucas <116270915+kevinlucasro@users.noreply.github.com>
2023-07-12 13:16:49 -0300

    Add files via upload

---
+++ app/metodoSort.js
@@ -0,0 +1,9 @@
+let btnOdenarPorPreco = document.getElementById('btnOrdenarPorPreco');
+
+btnOdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco);
+
+function ordenarLivrosPorPreco() {
+    let lirvosOrdenados = livros.sort((a, b) => a.preco - b.preco);
+
+    exibirOsLivrosNaTela(lirvosOrdenados);
+};
